0ec2585bd38faf5b2234353d86a5479dbec829e0
rusinchuk <rusinchuk@gmail.com>
2021-06-18 20:18:05 +0300

    Page Elements, Page Blocks and Value Objects Lesson_12_Homework_done @elpiankova

---
+++ page_objects/blocks/post.py
@@ -0,0 +1,34 @@
+from page_objects.locators import PostLocator
+from user_object import User
+
+
+class PostBlock:
+    def __init__(self, element):
+        self.element = element
+        # self.wait = WebDriverWait(driver, 5)
+
+    @property
+    def text(self):
+        return self.element.find_element(*PostLocator.POST_TEXT).text
+
+    @property
+    def time(self):
+        return self.element.find_element(*PostLocator.POST_TIME).text
+
+    @property
+    def user(self):
+        user_link = self.element.find_element(*PostLocator.POST_USER)
+        real_name = user_link.text
+        username = user_link.get_attribute("href").split("/")[-1]
+        return User(username=username, real_name=real_name)
+
+    @property
+    def like_bt(self):
+        return self.element.find_element(*PostLocator.LIKES_BUTTON)
+
+    def delete(self):
+        # TODO
+        pass
+
+    def add_like(self):
+       self.like_bt.click()
