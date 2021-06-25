4ed530244f579120737763f0aa8d668fb729f99f
rusinchuk <rusinchuk@gmail.com>
2021-06-25 20:37:21 +0300

    DB connection

---
+++ tests/test_login_positive.py
@@ -0,0 +1,10 @@
+def test_login(driver, app, user):
+    app.main_page.initiate_sigh_in()
+    assert app.sign_in_page.is_this_page()
+    app.sign_in_page.input_username(user.username)
+    app.sign_in_page.input_password(user.password)
+    app.sign_in_page.signin_click()
+    app.sign_in_page.wait_authentication()
+    assert app.dashboard_page.active_menu.text == "DASHBOARD"
+    assert app.dashboard_page.is_this_page()
+
